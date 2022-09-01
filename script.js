1dbcb726b2e1429e1c1f8830cd51841bf15ef45c
Victor Ly <ly.victor2000@gmail.com>
2022-09-01 17:34:51 +0200

    render array

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 "use strict";
+
+let queue = []; //empty array, will be filled with items
+let currentCustomers; //a random number between 0 and 32
+
+let barChart; //bars will be appended to this bar chart element
+
+document.addEventListener("DOMContentLoaded", init);
+
+//initialize
+function init() {
+  barChart = document.getElementById("bar-chart");
+
+  updateQueue();
+}
+
+//update the queue with the new number of customers
+function updateQueue() {
+  if (queue.length >= 40) {
+    queue.pop();
+  }
+
+  currentCustomers = Math.floor(Math.random() * 33); //assuming between 0 and 32 inclusive
+  queue.unshift(currentCustomers);
+  //console.log(queue);
+
+  displayQueue();
+
+  setTimeout(updateQueue, 1000);
+}
+
+//add a new bar to the bar chart
+function displayQueue() {
+  barChart.textContent = "";
+  queue.forEach((bar) => {
+    barChart.innerHTML += `<li class="bar" style="height: ${bar * 4}px; background-color: hsl(${
+      128 - bar * 4
+    }, 100%, 50%)"></li>`;
+  });
+}
